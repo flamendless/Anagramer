08144530b3d2e17349239b1b4a28fb201db3547e
flamendless <flamendless8@gmail.com>
2021-10-22 10:25:53 +0800

    Updated conf.lua to load ads module only when in Android

--- conf.lua
+++ conf.lua
@@ -3,27 +3,28 @@ function love.conf(t)
 	user = {}
 	_test = false
 
-	t.modules = {
-		ads = true,
-		data = true,
-		event = true,
-		keyboard = true,
-		mouse = true,
-		timer = true,
-		joystick = true,
-		touch = true,
-		image = true,
-		graphics = true,
-		audio = true,
-		math = true,
-		physics = true,
-		sound = true,
-		system = true,
-		font = true,
-		thread = true,
-		window = true,
-		video = true,
-	}
+	if love._os == "Android" then
+		t.modules.admob = true
+	end
+
+	t.modules.audio = true
+	t.modules.data = true
+	t.modules.event = true
+	t.modules.font = true
+	t.modules.graphics = true
+	t.modules.image = true
+	t.modules.joystick = false
+	t.modules.keyboard = true
+	t.modules.math = true
+	t.modules.mouse = true
+	t.modules.physics = false
+	t.modules.sound = true
+	t.modules.system = true
+	t.modules.thread = true
+	t.modules.timer = true
+	t.modules.touch = true
+	t.modules.video = false
+	t.modules.window = true
 
 	t.window.width = 480
 	t.window.height = 640
